c9727c1871a95cf106b6dbe2df513b0c64134282
lyashenko.ivan <ivan.lyashenko.it@gmail.com>
2026-09-17 16:23:28 +0300

    init project and add first smoke test

---
+++ tests/ui/home.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("/");
+
+  const header = page.locator("header");
+
+  await expect(
+    header.getByRole("img", { name: "Website for automation practice" }),
+  ).toBeVisible();
+
+  const navigationLinks = [
+    { name: "Home", href: "/" },
+    { name: "Products", href: "/products" },
+    { name: "Cart", href: "/view_cart" },
+    { name: "Signup / Login", href: "/login" },
+    { name: "Test Cases", href: "/test_cases" },
+    { name: "API Testing", href: "/api_list" },
+    {
+      name: "Video Tutorials",
+      href: "https://www.youtube.com/c/AutomationExercise",
+    },
+    { name: "Contact us", href: "/contact_us" },
+  ];
+
+  const menu = header.getByRole("list");
+
+  for (const { name, href } of navigationLinks) {
+    const link = menu.getByRole("link").filter({ hasText: name });
+
+    await expect(link).toHaveCount(1);
+    await expect(link).toBeVisible();
+    await expect(link).toHaveAttribute("href", href);
+  }
+});
